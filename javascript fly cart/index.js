14873c5edd4818e86b508bcc65865aa75ead6812
awolu owolabi samuel <58808750+Samuel3310@users.noreply.github.com>
2023-08-25 22:50:53 +0100

    Flying cart using vanilla javascript

---
+++ javascript fly cart/index.js
@@ -0,0 +1,33 @@
+
+const arr = [  '', 'phone lite', 'headset one', 'HP Spectre x360 15', 'another phone', 'laptop', 'clean shoe', 'airplane headset', 'yellow shoe']
+const price = [null, '$200', '$300', '$350', '$70', '$1000', '$908', '$323', '$1223','$400']
+ const loading = ()=>{
+    let slider = document.querySelector('.slider')
+    let counter = 9;
+    
+    for(let i = 1; i < counter; i++){
+    slider.innerHTML +=  `
+        <div class="card">
+
+            <div class="card-content">
+                <img src="images/${i}.png" alt="" class="card-img">
+                <h1 class="card-title">${arr[i]}</h1>
+                <div class="card-body">
+                    <div class="card-star">
+                        <span class="rating-value">4.5</span>
+                        <span class="star">&#9733;</span>
+                    </div>
+                    <p class="card-price">${price[i]}</p>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-success">Buy Now</button>
+                    <button class="btn btn-border add-to-cart">Add To Cart</button>
+                </div>
+            </div>
+        </div>
+        `
+      
+    }
+}
+
+loading()
