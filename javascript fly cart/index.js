05bcd8ad019ab3ddcd0d6719db378352ffd92e96
awolu owolabi samuel <58808750+Samuel3310@users.noreply.github.com>
2023-08-25 23:59:47 +0100

    Update index.js

--- javascript fly cart/index.js
+++ javascript fly cart/index.js
@@ -3,7 +3,7 @@ const arr = [  '', 'phone lite', 'headset one', 'HP Spectre x360 15', 'another p
 const price = [null, '$200', '$300', '$350', '$70', '$1000', '$908', '$323', '$1223','$400']
  const loading = ()=>{
     let slider = document.querySelector('.slider')
-    let counter = 0;
+    let counter = 1;
     
     for(let i = 9; i > counter; i--){
     slider.innerHTML +=  `
